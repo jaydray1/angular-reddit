9b176e03f01dc5a31b9c4dea93b7763811595753
JM <jdreher15@gmail.com>
2018-03-06 19:43:11 -0700

    model

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {SuiModule} from 'ng2-semantic-ui';
 
 
 import { AppComponent } from './app.component';
+import { ArticleComponent } from './article/article.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ArticleComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    SuiModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/article/article.component.ts
+++ src/app/article/article.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, HostBinding } from '@angular/core';
+import { Article } from '../article/article.model';
 
 @Component({
   selector: 'app-article',
@@ -7,23 +8,24 @@ import { Component, OnInit, HostBinding } from '@angular/core';
 })
 export class ArticleComponent implements OnInit {
   @HostBinding('attr.class') cssClass = 'row';
-  votes: number;
-  title: string;
-  link: string;
+  article: Article;
 
   constructor() {
-    this.title = 'Angular 2';
-    this.link = 'http://angular.io';
-    this.votes = 10;
+    this.article = new Article(
+      'Angular 2',
+      'http://angular.io',
+      10);
    }
-   voteUp() {
-     this.votes += 1;
+   voteUp(): boolean {
+     this.article.voteUp();
+     return false;
    }
-   voteDown() {
-     this.votes -= 1;
+   voteDown(): boolean {
+     this.article.voteDown();
+     return false;
    }
+   ngOnInit() {
+   }
+ 
+ }
     
-  ngOnInit() {
-  }
-
-}
